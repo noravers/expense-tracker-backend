1b4675a6ca8602015ccc05839a046a113ddbbba7
noravers <gia.drago4@gmail.com>
2023-06-05 19:26:01 -0600

    error mongoose

--- db/db.js
+++ db/db.js
@@ -2,12 +2,11 @@ const moongose = require('mongoose')
 
 const db = async() => {
     try{
-        console.log(moongose, 'mongoose')
         moongose.set('strictQuery', false)
         await moongose.connect(process.env.MONGO_URL)
         console.log('Db connected')
     }catch(error){
-        console.log(`'DB Connection Error ${process.env.MONGO_URL}`, error)
+        console.log(`'DB Connection Error`, error)
     }
 }
 

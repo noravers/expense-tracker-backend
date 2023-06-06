41f3ee6e9f0c29125dda802962f4de4de72e7aaa
noravers <gia.drago4@gmail.com>
2023-06-05 19:18:20 -0600

    m

--- db/db.js
+++ db/db.js
@@ -7,7 +7,7 @@ const db = async() => {
         await moongose.connect(process.env.MONGO_URL)
         console.log('Db connected')
     }catch(error){
-        console.log('DB Connection Error', error)
+        console.log(`'DB Connection Error ${process.env.MONGO_URL}`, error)
     }
 }
 
